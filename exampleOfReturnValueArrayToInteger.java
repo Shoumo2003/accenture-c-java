7a00e99ef96bcbefed4497bcba9ff332649b3dea
Shoumo Mondal <shoumomondal@gmail.com>
2024-09-01 23:35:56 +0530

    RETURN VALUE ARRAY TO INTEGER

---
+++ exampleOfReturnValueArrayToInteger.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        int n = nums.length;
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            rightSum += nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            rightSum -= nums[i];
+            ans.add(Math.abs(leftSum - rightSum));
+            leftSum += nums[i];
+        }
+
+        // Convert the list to an array
+        return ans.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
